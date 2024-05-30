c3b8062262db60a09c3e30c0c97dd0406d321d00
John Kipruto <johnkipruto157@gmail.com>
2024-05-30 18:00:49 +0300

    Edit vercel configurations

--- mock_server/index.js
+++ mock_server/index.js
@@ -3,11 +3,6 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db/db.json");
 
-// Make sure to use the default middleware
-const middlewares = jsonServer.defaults();
-
-server.use(middlewares);
-// Add this before server.use(router)
 
 server.listen(3000, () => {
  console.log("JSON Server is running");
